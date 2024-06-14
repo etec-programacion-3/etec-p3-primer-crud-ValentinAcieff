f601aadbada89373f9d87291b8a2af1bc2fe728b
ValentinAcieff <a.acieff@alumno.etec.um.edu.ar>
2024-06-14 13:05:57 -0300

    hola

---
+++ index.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Sequelize, Model, DataTypes } from 'sequelize';
+
+const app = express();
+const port = 3000;
+
+const filename = "database.db"
+console.log(filename)
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: filename
+});
+
+class Book extends Model { }
+Book.init({
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    genre: DataTypes.STRING
+}, { sequelize, modelName: 'book' });
+
+sequelize.sync();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/books', async (req, res) => {
+    const books = await Book.findAll();
+    res.json(books);
+});
+
+app.get('/books/:id', async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    res.json(book);
+});
+
+app.post('/books', async (req, res) => {
+    const book = await Book.create(req.body);
+    res.json(book);
+});
+
+app.put('/books/:id', async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+        await book.update(req.body);
+        res.json(book);
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+});
+
+app.delete('/books/:id', async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+        await book.destroy();
+        res.json({ message: 'Book deleted' });
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
